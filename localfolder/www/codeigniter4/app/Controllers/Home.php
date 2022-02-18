27bf31ad96a6db55b11b6b0b53edda2186a505a8
kadal-sawah <riz.idn.dev@gmail.com>
2022-02-18 14:45:29 +0700

    #added portal template layouting

--- localfolder/www/codeigniter4/app/Controllers/Home.php
+++ localfolder/www/codeigniter4/app/Controllers/Home.php
@@ -6,6 +6,15 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        return view('home');
     }
+
+    public function beranda($data = null)
+    {
+        $data = [];
+        $data['content'] = 'beranda';
+        // return view('layouts/content', $data, ['cache' => 120]);
+        return view('layouts/content', $data);
+    }
+
 }
---
+++ localfolder/www/codeigniter4/app/Views/layouts/content.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+
+  <?php echo $this->include('layouts/header');?>
+
+</head> 
+
+<body class="app">   	
+
+  <header class="app-header fixed-top">
+
+    <?php echo $this->include('layouts/navbar');?>
+
+      <?php echo $this->include('layouts/sidebar');?>
+    
+  </header><!--//app-header-->
+    
+  <div class="app-wrapper">
+
+    <?php echo $this->section('content');?>
+    
+    <?php echo $this->include('layouts/footer');?>
+    
+  </div><!--//app-wrapper-->    					
+
+  <?php echo $this->include('layouts/script');?>
+
+</body>
+</html> 
+
